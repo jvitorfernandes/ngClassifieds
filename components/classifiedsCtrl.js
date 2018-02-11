3c9842d1c164106ba83c9ba04d5b2f537355f7aa
João <fernandesjvb@gmail.com>
2018-02-11 17:05:09 -0600

    adding and editing

--- components/classifiedsCtrl.js
+++ components/classifiedsCtrl.js
@@ -1,9 +1,46 @@
 
-const classifiedsCtrl = app.controller('classifiedsCtrl', ['$scope', '$http', 'classifiedsFac', function($scope, $http, classifiedsFac){
+const classifiedsCtrl = 
+      app.controller('classifiedsCtrl', 
+                    ['$scope', '$http', 'classifiedsFac', '$mdSidenav', '$mdToast', 
+                    function($scope, $http, classifiedsFac, $mdSidenav, $mdToast){
+
     
     classifiedsFac.getClassifieds().then(function(classifieds){
         $scope.classifieds = classifieds.data;
     });
     
-
+    $scope.toggleSideBar = function(){
+        $mdSidenav('left').toggle();
+    };
+    
+    $scope.saveClassified = function(classified){
+        if(classified){
+            $scope.classifieds.push(classified);
+            $scope.classified = {};
+            $scope.toggleSideBar();
+            showToast("Classified saved");
+        };
+    };
+    
+    function showToast(message){
+        $mdToast.show($mdToast.simple()
+                        .content(message)
+                        .position('top, right')
+                        .hideDelay(3000)
+                    );
+    };
+    
+    $scope.editClassified = function(classified){
+        $scope.editing = true;
+        $scope.toggleSideBar();
+        $scope.classified = classified;
+    };
+                        
+    $scope.saveEdit = function(){
+        $scope.editing = false;
+        $scope.classified = {};
+        $scope.toggleSideBar();
+        showToast("Edit saved");
+    }
+                        
 }]);
